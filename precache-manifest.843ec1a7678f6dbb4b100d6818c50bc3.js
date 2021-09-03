34935a90d9aefefda81c6f101fc1c0b4b108a97c
wayy <948840255@qq.com>
2021-09-03 14:55:54 +0800

    deploy

---
+++ precache-manifest.843ec1a7678f6dbb4b100d6818c50bc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "533364e7898ffe470877",
+    "url": "/money-1-website/css/app.d59a81d9.css"
+  },
+  {
+    "revision": "42a3a6eac2b50585b8c25143e4995447",
+    "url": "/money-1-website/index.html"
+  },
+  {
+    "revision": "533364e7898ffe470877",
+    "url": "/money-1-website/js/app.4ef3cfb6.js"
+  },
+  {
+    "revision": "7716c195ccd91595b01d",
+    "url": "/money-1-website/js/chunk-vendors.4e887dfd.js"
+  },
+  {
+    "revision": "f3aaee380deddea3de78c3de33e81ce5",
+    "url": "/money-1-website/manifest.json"
+  },
+  {
+    "revision": "00003ecd86ba22c79317aa15a659babc",
+    "url": "/money-1-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-1-website/robots.txt"
+  }
+]);
